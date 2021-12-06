4a0f20a16aae0d225b32e3f17f3d80e8fb3e4570
oscar-gong <oscargong00@gmail.com>
2021-12-06 12:26:25 +1100

    Completed poker exercise

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -8,53 +10,115 @@ public class App {
         // Take in hands from command line input then form a list using split
 
         Scanner scanner = new Scanner(System.in);
+        int p1WinCount = 0;
+        int p2WinCount = 0;
         while(scanner.hasNextLine()){
             String cards = scanner.nextLine();
             String[] player1 = cards.substring(0,14).split(" ");
             String[] player2 = cards.substring(15,29).split(" ");
             Hand playerHand1 = new Hand(player1);
-            System.out.println(playerHand1.hasRoyalFlush());
-            // for(String a: player1){
-            //     System.out.println(a);
-            // }
-
-
+            Hand playerHand2 = new Hand(player2);
+            int winner = findWinner(playerHand1, playerHand2);
+            if(winner == 1) p1WinCount++;
+            if(winner == 2) p2WinCount++;
         }
         scanner.close();
 
-        System.out.println("Hello, World!");
+        System.out.println("Player 1: " + p1WinCount);
+        System.out.println("Player 2: " + p2WinCount);
     }
 
-    // Make function to check each combination
+    private static int findWinner(Hand player1, Hand player2) {
+        int player1Rank = player1.getHandRank();
+        int player2Rank = player2.getHandRank();
 
-    // private static int isRoyalFlush(String[] hand){
-    //     char suit = hand[0].charAt(1);
-    //     for(String card: hand){
-    //         if(suit != card.charAt(1)){
-    //             return 0;
-    //         }
-    //     }
-    //     Set<String> set = new HashSet<>();
-    //     set.add("one");
-    //     set.add("two");
-    //     set.add("three");
+        if(player1Rank > player2Rank) return 1;
+        if(player2Rank > player1Rank) return 2;
+        
 
+        if(player1Rank == 10) {
+            return 0;
+        } else if(player1Rank == 9) {
+            int p1 = player1.hasStraightFlush();
+            int p2 = player2.hasStraightFlush();
 
-    //     return 1;
-    // }
+            if(p1 == p2) return 0;
 
+            return p1>p2 ? 1 : 2;
 
+        } else if(player1Rank == 8) {
+            List<Integer> player1CheckOrder = player1.hasFourOfAKind();
+            List<Integer> player2CheckOrder = player2.hasFourOfAKind();
+            for(int i = 0; i<2; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+
+            return 0;
+        } else if(player1Rank == 7) {
+            List<Integer> player1CheckOrder = player1.hasFullHouse();
+            List<Integer> player2CheckOrder = player2.hasFullHouse();
+            for(int i = 0; i<2; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+
+            return 0;
+        } else if(player1Rank == 6) {
+            List<Integer> player1CheckOrder = player1.hasFlush();
+            List<Integer> player2CheckOrder = player2.hasFlush();
+            for(int i = 0; i<5; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+
+            return 0;
+        } else if(player1Rank == 5) {
+            int p1 = player1.hasStraight();
+            int p2 = player1.hasStraight();
+
+            if(p1 == p2) return 0;
+
+            return p1>p2 ? 1 : 2;
+        } else if(player1Rank == 4) {
+            List<Integer> player1CheckOrder = player1.hasThreeOfAKind();
+            List<Integer> player2CheckOrder = player2.hasThreeOfAKind();
+            for(int i = 0; i<3; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+
+            return 0;
+        } else if(player1Rank == 3) {
+            List<Integer> player1CheckOrder = player1.hasTwoPairs();
+            List<Integer> player2CheckOrder = player2.hasTwoPairs();
+            for(int i = 0; i<3; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+
+            return 0;
+        } else if(player1Rank == 2) {
+            List<Integer> player1CheckOrder = player1.hasPair();
+            List<Integer> player2CheckOrder = player2.hasPair();
+            for(int i = 0; i<4; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+
+            return 0;
+        } else if(player1Rank == 1) {
+            List<Integer> player1CheckOrder = player1.getValueInDescOrder();
+            List<Integer> player2CheckOrder = player2.getValueInDescOrder();
+            for(int i = 0; i<5; i++) {
+                if(player1CheckOrder.get(i) > player2CheckOrder.get(i)) return 1;
+                if(player1CheckOrder.get(i) < player2CheckOrder.get(i)) return 2;
+            }
+            return 0;
+        }
+
+        return 0;
+    }
     // Make function to compare two hands
 
-    // Sort hand
-    // private static int isRoyalFlush(String[] hand){
-    //     char suit = hand[0].charAt(1);
-    //     for(String card: hand){
-    //         if(suit != card.charAt(1)){
-    //             return 0;
-    //         }
-    //     }
-
-    //     return 1;
-    // }
 }
--- src/Hand.java
+++ src/Hand.java
@@ -7,6 +7,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.swing.ListModel;
+
 // instead of returning true false, maybe return highest value card of rank or -1 if condition is false
 
 public class Hand {
@@ -26,68 +28,82 @@ public class Hand {
 
     public boolean hasRoyalFlush() {
         // Check if all cards have the same suit
-        if(!hasFlush()) return false;
+        if(hasFlush() == null) return false;
 
-        Set<Character> royalFlushValue = new HashSet<>();
-        royalFlushValue.add('T');
-        royalFlushValue.add('J');
-        royalFlushValue.add('Q');
-        royalFlushValue.add('K');
-        royalFlushValue.add('A');
+        // Old solution for unsorted hand
+        // Set<Character> royalFlushValue = new HashSet<>();
+        // royalFlushValue.add('T');
+        // royalFlushValue.add('J');
+        // royalFlushValue.add('Q');
+        // royalFlushValue.add('K');
+        // royalFlushValue.add('A');
 
-        // Add values of hand in set
-        Set<Character> values = new HashSet<>();
-        for(Card card:this.cards) {
-            values.add(card.getValue());
-        }
+        // // Add values of hand in set
+        // Set<Character> values = new HashSet<>();
+        // for(Card card:this.cards) {
+        //     values.add(card.getValue());
+        // }
 
-        // If the hand consists of 10 to Ace values
-        return royalFlushValue.equals(values);
+        // // If the hand consists of 10 to Ace values
+        // return royalFlushValue.equals(values);
+
+        
+        // Since hand is sorted, the highest and lowest hand should be T and A
+        // assuming that there are no duplicate of the same value and suit.
+        return cards.get(0).getValue() == 'T' && cards.get(4).getValue() == 'A';
     } 
 
-    public boolean hasStraightFlush() {
-        return hasStraight() && hasFlush();
+    public int hasStraightFlush() {
+        return (hasStraight() != 0 && hasFlush() != null) ? hasStraightFlush() : 0;
     } 
       
-    public boolean hasFourOfAKind() {
+    public List<Integer> hasFourOfAKind() {
         return hasXOfAKind(4);
     } 
 
-    public boolean hasFullHouse() {
-        return hasPair() && hasThreeOfAKind();
+    public List<Integer> hasFullHouse() {
+        return hasPair() != null && hasThreeOfAKind() != null ? hasThreeOfAKind() : null;
     }   
 
-    public boolean hasFlush() {
+    public List<Integer> hasFlush() {
         char suit = this.cards.get(0).getSuit();
+
+        ArrayList<Integer> checkOrder = new ArrayList<Integer>();
 
         for(Card card:this.cards) {
             if(card.getSuit() != suit)
-                return false;
+                return null;
+            checkOrder.add(getIntValue(card.getValue()));
         }
-        return true;
+        Collections.sort(checkOrder, Collections.reverseOrder());
+
+
+        return checkOrder;
     }
 
-    public boolean hasStraight() {
+    // Returns 0 if no straight or highest value
+    public int hasStraight() {
         List<Character> valueOrder = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A');
         
         char lowestValue = cards.get(0).getValue();
         int lowestIndex = valueOrder.indexOf(lowestValue);
 
         // If the lowest value is above 10 (9th index), it is impossible to get a straightflush
-        if(lowestIndex > 9) return false;
+        if(lowestIndex > 9) return 0;
 
         for(int i = 0; i < 5; i++) {
             if(valueOrder.get(lowestIndex+i) != cards.get(i).getValue())
-                return false;
+                return 0;
         }
-        return true;
+
+        return cards.get(4).getValue();
     }
         
-    public boolean hasThreeOfAKind() {
+    public List<Integer> hasThreeOfAKind() {
         return hasXOfAKind(3);
     }
         
-    public boolean hasTwoPairs() {
+    public List<Integer> hasTwoPairs() {
         HashMap<Character, Integer> valueCount = new HashMap<Character, Integer>();
 
         for(Card card:cards) {
@@ -98,14 +114,29 @@ public class Hand {
             }
         }
 
-        return Collections.frequency(valueCount.values(), 2) == 2;
+        ArrayList<Integer> checkOrder = new ArrayList<Integer>();
+
+        if(Collections.frequency(valueCount.values(), 2) != 2) return null;
+
+        for(Character value: valueCount.keySet()) {
+            if(valueCount.get(value) == 2) {
+                checkOrder.add(getIntValue(value));
+            }
+        }
+        Collections.sort(checkOrder, Collections.reverseOrder());
+        for(Character value: valueCount.keySet()) {
+            if(valueCount.get(value) == 1) {
+                checkOrder.add(getIntValue(value));
+            }
+        }
+        return checkOrder;
     }
         
-    public boolean hasPair() {
+    public List<Integer> hasPair() {
         return hasXOfAKind(2);
     }
 
-    public boolean hasXOfAKind(int x) {
+    public List<Integer> hasXOfAKind(int x) {
         HashMap<Character, Integer> valueCount = new HashMap<Character, Integer>();
 
         for(Card card:cards) {
@@ -116,7 +147,70 @@ public class Hand {
             }
         }
 
-        return valueCount.containsValue(x);
+        ArrayList<Integer> checkOrder = new ArrayList<Integer>();
+
+        int firstCheck = -1;
+
+        for(Character value: valueCount.keySet()) {
+            if(valueCount.get(value) == x) {
+                firstCheck = getIntValue(value);
+            }
+        }
+
+        for(Card card:cards) {
+            if(getIntValue(card.getValue()) != firstCheck) {
+                checkOrder.add(getIntValue(card.getValue()));
+            }
+        }
+        
+        if(firstCheck == -1) return null;
+
+        Collections.sort(checkOrder, Collections.reverseOrder());
+        checkOrder.add(0, firstCheck);
+
+        return checkOrder;
+    }
+
+    public List<Integer> getValueInDescOrder() {
+        ArrayList<Integer> checkOrder = new ArrayList<Integer>();
+        for(Card card: cards) {
+            checkOrder.add(getIntValue(card.getValue()));
+        }
+        Collections.sort(checkOrder, Collections.reverseOrder());
+        return checkOrder;
+    }
+
+    public int getHandRank() {
+        int rank = 1;
+
+        if(hasPair() != null) rank = 2;
+        if(hasTwoPairs() != null)  rank = 3;
+        if(hasThreeOfAKind() != null) rank = 4;
+        if(hasStraight() != 0) rank = 5;
+        if(hasFlush() != null) rank = 6;
+        if(hasFullHouse() != null) rank = 7;
+        if(hasFourOfAKind() != null) rank = 8;
+        if(hasStraightFlush() != 0) rank = 9;
+        if(hasRoyalFlush()) rank = 10;
+
+        return rank;
+    }
+
+    public int getIntValue(char value) {
+        if(value == '2') return 2;
+        if(value == '3') return 3;
+        if(value == '4') return 4;
+        if(value == '5') return 5;
+        if(value == '6') return 6;
+        if(value == '7') return 7;
+        if(value == '8') return 8;
+        if(value == '9') return 9;
+        if(value == 'T') return 10;
+        if(value == 'J') return 11;
+        if(value == 'Q') return 12;
+        if(value == 'K') return 13;
+        if(value == 'A') return 14;
+        return 0;
     }
 
     public String toString() {
